7f9bb77a68fa692f3918d37b3b0f9a00a9102b6c
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-07 10:11:00 -0300

    add departments crud scaffolding

---
+++ sales-mvc/Data/SalesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using sales_mvc.Models;
+
+namespace sales_mvc.Data
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext (DbContextOptions<SalesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<sales_mvc.Models.Department> Department { get; set; }
+    }
+}
--- sales-mvc/Startup.cs
+++ sales-mvc/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using sales_mvc.Data;
 
 namespace sales_mvc
 {
@@ -33,6 +35,9 @@ namespace sales_mvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
