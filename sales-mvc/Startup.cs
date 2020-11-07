adb8505806798c7901aa0ba4474418675bc3491a
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-07 10:46:29 -0300

    config mysql

--- sales-mvc/Startup.cs
+++ sales-mvc/Startup.cs
@@ -37,7 +37,8 @@ namespace sales_mvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesContext"), builder =>
+                    builder.MigrationsAssembly("sales_mvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
