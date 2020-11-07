19590af60f317e01ea21b4070e341f4a3379d2fd
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-07 15:25:51 -0300

    add insert method on seller service

--- sales-mvc/Services/SellerService.cs
+++ sales-mvc/Services/SellerService.cs
@@ -20,5 +20,11 @@ namespace sales_mvc.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller seller)
+        {
+            _context.Add(seller);
+            _context.SaveChanges();
+        }
     }
 }
