78e2007b5e8c5f2b31b8329510872e0ac1c4874d
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-09 09:29:37 -0300

    add exception errors

---
+++ sales-mvc/Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sales_mvc.Services.Exceptions
+{
+    public class DbConcurrencyException : ApplicationException
+    {
+        public DbConcurrencyException(string message) : base(message) { }
+    }
+}
---
+++ sales-mvc/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sales_mvc.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
