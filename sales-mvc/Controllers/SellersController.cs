551c1102c157e930d395da2f6722484b5d72eb58
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-09 11:40:59 -0300

    add validation

--- sales-mvc/Controllers/SellersController.cs
+++ sales-mvc/Controllers/SellersController.cs
@@ -38,6 +38,11 @@ namespace sales_mvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(seller);
+            }
+
             _sellerService.Insert(seller);
 
             return RedirectToAction(nameof(Index));
@@ -110,6 +115,14 @@ namespace sales_mvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+
+                return View(viewModel);
+            }
+
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch." });
--- sales-mvc/Models/Seller.cs
+++ sales-mvc/Models/Seller.cs
@@ -8,23 +8,33 @@ namespace sales_mvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} is required.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} is required.")]
+        [EmailAddress(ErrorMessage = "Enter a valid email.")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} is required.")]
         [Display(Name = "Birth Date")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} is required.")]
+        [Range(100, 100000, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DataType(DataType.Currency)]
         public double BaseSalary { get; set; }
+        
         public Department Department { get; set; }
 
         [Display(Name = "Department")]
         public int DepartmentId { get; set; }
+        
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller() { }
